af0777347a8ae0318764bc3236e3d74f6a056852
Vinayak Gupta <guptavinayak1614@gmail.com>
2025-06-25 17:37:44 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -15,7 +15,7 @@ export default function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/signup", form);
+      await axios.post("https://online-lecture-scheduling-module-6nzr.onrender.com/api/auth/signup", form);
       setMessage("✅ Signup successful! Redirecting to login...");
       setTimeout(() => {
         navigate("/login");
